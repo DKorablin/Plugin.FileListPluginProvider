772297ae5fa840f5be177c57278895e3d412509a
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 13:44:02 +0300

    Unifying build process

--- Plugin.FileListPluginProvider/Properties/AssemblyInfo.cs
+++ Plugin.FileListPluginProvider/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("d505490e-747c-4d05-becd-8661b85c1711")]
 [assembly: System.CLSCompliant(true)]
 
@@ -11,12 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Plugin.FileListPluginProvider")]
 [assembly: AssemblyDescription("Plugin provider using XML file to specify assemblies and instances to load")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
 [assembly: AssemblyProduct("Plugin loader from file system by list")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
 #endif
